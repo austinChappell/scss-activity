8ec5247418cb1c8dba60753790d3ab8c5e5b8ffb
austinChappell <Austin.W.Chappell@gmail.com>
2017-08-17 10:14:19 -0500

    env message

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ let port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
+console.log('===============================================');
+console.log(process.env);
+
 app.listen(port, (req, res) => {
   console.log(`Listening on PORT ${ port }.`);
 });
